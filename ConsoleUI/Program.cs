9a910c98635a44d32579329151c78ebfa64fe575
DESKTOP-GG6IUDJ\enes_ <yslyrt.enes@gmail.com>
2021-08-15 23:47:14 +0300

    +Refactor

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 
@@ -9,8 +10,8 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
-            foreach (var item in productManager.GetAll())
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var item in productManager.GetAllByCategoryId(2))
             {
                 Console.WriteLine(item.ProductName);
             }
--- Entities/Concrete/Customer.cs
+++ Entities/Concrete/Customer.cs
@@ -7,7 +7,7 @@ namespace Entities.Concrete
 {
    public class Customer:IEntity
     {
-        public string CustomerId { get; set; }
+        public string CustomerID { get; set; }
         public string ContactName { get; set; }
         public string CompanyName { get; set; }
         public string City { get; set; }
