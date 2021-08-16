b6b46fa12e65e6e7703d9e95b0c618ecb042ff06
DESKTOP-GG6IUDJ\enes_ <yslyrt.enes@gmail.com>
2021-08-16 21:47:44 +0300

    +refactor business

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,10 +10,25 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var item in productManager.GetAllByCategoryId(2))
+             ProductTest();
+            //CategoryTest();
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var item in categoryManager.GetAll())
             {
-                Console.WriteLine(item.ProductName);
+                Console.WriteLine(item.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var item in productManager.GetProductDetails())
+            {
+                Console.WriteLine(item.CategoryName);
             }
         }
     }
