77c27401029be2553d883cf07c1bd2d46b199ec7
DESKTOP-GG6IUDJ\enes_ <yslyrt.enes@gmail.com>
2021-08-21 20:12:24 +0300

    +Core Module

---
+++ Core/DependencyResolvers/CoreModule.cs
@@ -0,0 +1,21 @@
+﻿using Core.CrossCutingConcerns.Caching;
+using Core.CrossCutingConcerns.Caching.Microsoft;
+using Core.Utilities.IoC;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.DependencyResolvers
+{
+    public class CoreModule : ICoreModule
+    {
+        public void Load(ServiceCollection serviceColection)
+        {
+            serviceColection.AddMemoryCache();
+            serviceColection.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            serviceColection.AddSingleton<ICacheManager, MemoryCacheManager>();
+        }
+    }
+}
